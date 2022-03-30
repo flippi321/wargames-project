8a8fee94c1cf7efb40ba34501c41816b2038f18c
flippi321 <90700334+flippi321@users.noreply.github.com>
2022-03-30 14:15:04 +0200

    Sorted UnitTests with @Nested

--- src/test/java/CommanderUnitTest.java
+++ src/test/java/CommanderUnitTest.java
@@ -1,53 +1,62 @@
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CommanderUnitTest {
-    @Test
-    @DisplayName("Testing for correct automatic armour values")
-    public void checkCommanderAutomaticArmourValues() {
-        try {
-            Unit newCommander = new CommanderUnit("Generic General", 150);
-            assertEquals(15, newCommander.getArmour());
-        } catch (Exception e) {
-            fail("checkCommanderAutomaticArmourValues failed");
+    @Nested
+    @DisplayName("Constructors")
+    class constructors{
+        @Test
+        @DisplayName("Testing for correct automatic armour values")
+        public void checkCommanderAutomaticArmourValues() {
+            try {
+                Unit newCommander = new CommanderUnit("Generic General", 150);
+                assertEquals(15, newCommander.getArmour());
+            } catch (Exception e) {
+                fail("checkCommanderAutomaticArmourValues failed");
+            }
+        }
+
+        @Test
+        @DisplayName("Testing for correct automatic attack values")
+        public void checkCommanderAutomaticAttackValues() {
+            try {
+                Unit newCommander = new CommanderUnit("Generic General", 180);
+                assertEquals(25, newCommander.getAttack());
+            } catch (Exception e) {
+                fail("checkCommanderAutomaticAttackValues failed");
+            }
+        }
+
+        @Test
+        @DisplayName("Testing extended Constructor")
+        public void checkCommanderName() {
+            try {
+                Unit newCommander = new CommanderUnit("High King", 200, 10, 15);
+                assertEquals("High King", newCommander.getName());
+            } catch (Exception e) {
+                fail("checkCommanderName failed");
+            }
         }
     }
 
-    @Test
-    @DisplayName("Testing for correct automatic attack values")
-    public void checkCommanderAutomaticAttackValues() {
-        try {
-            Unit newCommander = new CommanderUnit("Generic General", 180);
-            assertEquals(25, newCommander.getAttack());
-        } catch (Exception e) {
-            fail("checkCommanderAutomaticAttackValues failed");
-        }
-    }
-
-    @Test
-    @DisplayName("Testing extended Constructor")
-    public void checkCommanderName() {
-        try {
-            Unit newCommander = new CommanderUnit("High King", 200, 10, 15);
-            assertEquals("High King", newCommander.getName());
-        } catch (Exception e) {
-            fail("checkCommanderName failed");
-        }
-    }
-
-    @Test
-    @DisplayName("Testing Defence and attack")
-    public void checkFighting() {
-        try {
-            int originalHealth = 180;
-            Unit commanderDefender = new CavalryUnit("Mongol Warlord", originalHealth);
-            Unit commanderAggressor = new CavalryUnit("Queen Consort", 150);
-            commanderAggressor.attack(commanderDefender);
-            assertTrue((commanderDefender.getHealth() < originalHealth));
-        } catch (Exception e) {
-            fail("checkInfantryName failed");
+    @Nested
+    @DisplayName("Combat Stats")
+    class combatStats   {
+        @Test
+        @DisplayName("Testing Defence and attack")
+        public void checkFighting() {
+            try {
+                int originalHealth = 180;
+                Unit commanderDefender = new CavalryUnit("Mongol Warlord", originalHealth);
+                Unit commanderAggressor = new CavalryUnit("Queen Consort", 150);
+                commanderAggressor.attack(commanderDefender);
+                assertTrue((commanderDefender.getHealth() < originalHealth));
+            } catch (Exception e) {
+                fail("checkInfantryName failed");
+            }
         }
     }
 }
--- src/test/java/InfantryUnitTest.java
+++ src/test/java/InfantryUnitTest.java
@@ -1,64 +1,73 @@
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class InfantryUnitTest {
-    @Test
-    @DisplayName("Testing for correct automatic armour values")
-    public void checkInfantryAutomaticArmourValues() {
-        try {
-            Unit newInfantry = new InfantryUnit("Generic Spearman", 10);
-            assertEquals(10, newInfantry.getArmour());
-        } catch (Exception e) {
-            fail("checkInfantryAutomaticValues failed");
+    @Nested
+    @DisplayName("Constructors")
+    class constructors {
+        @Test
+        @DisplayName("Testing for correct automatic armour values")
+        public void checkInfantryAutomaticArmourValues() {
+            try {
+                Unit newInfantry = new InfantryUnit("Generic Spearman", 10);
+                assertEquals(10, newInfantry.getArmour());
+            } catch (Exception e) {
+                fail("checkInfantryAutomaticValues failed");
+            }
+        }
+
+        @Test
+        @DisplayName("Testing for correct automatic armour values")
+        public void checkInfantryAutomaticAttackValues() {
+            try {
+                Unit newInfantry = new InfantryUnit("Generic Spearman", 10);
+                assertEquals(15, newInfantry.getAttack());
+            } catch (Exception e) {
+                fail("checkInfantryAutomaticValues failed");
+            }
+        }
+
+        @Test
+        @DisplayName("Testing extended Constructor")
+        public void checkInfantryConstructor() {
+            try {
+                Unit newInfantry = new InfantryUnit("Kings Guard", 100, 2, 5);
+                assertEquals(5, newInfantry.getArmour());
+            } catch (Exception e) {
+                fail("checkInfantryConstructor failed");
+            }
+        }
+
+        @Test
+        @DisplayName("Testing name is correct")
+        public void checkInfantryName() {
+            try {
+                Unit newInfantry = new InfantryUnit("Swordsman", 100);
+                assertEquals("Swordsman", newInfantry.getName());
+            } catch (Exception e) {
+                fail("checkInfantryName failed");
+            }
         }
     }
 
-    @Test
-    @DisplayName("Testing for correct automatic armour values")
-    public void checkInfantryAutomaticAttackValues() {
-        try {
-            Unit newInfantry = new InfantryUnit("Generic Spearman", 10);
-            assertEquals(15, newInfantry.getAttack());
-        } catch (Exception e) {
-            fail("checkInfantryAutomaticValues failed");
-        }
-    }
-
-    @Test
-    @DisplayName("Testing extended Constructor")
-    public void checkInfantryConstructor() {
-        try {
-            Unit newInfantry = new InfantryUnit("Kings Guard", 100, 2, 5);
-            assertEquals(5, newInfantry.getArmour());
-        } catch (Exception e) {
-            fail("checkInfantryConstructor failed");
-        }
-    }
-
-    @Test
-    @DisplayName("Testing name is correct")
-    public void checkInfantryName() {
-        try {
-            Unit newInfantry = new InfantryUnit("Swordsman", 100);
-            assertEquals("Swordsman", newInfantry.getName());
-        } catch (Exception e) {
-            fail("checkInfantryName failed");
-        }
-    }
-
-    @Test
-    @DisplayName("Testing Defence and attack")
-    public void checkFighting() {
-        try {
-            int originalHealth = 150;
-            Unit infantryDefender = new InfantryUnit("Town Guard", originalHealth);
-            Unit infantryAggressor = new InfantryUnit("Bandit", 100);
-            infantryAggressor.attack(infantryDefender);
-            assertTrue((infantryDefender.getHealth() < originalHealth));
-        } catch (Exception e) {
-            fail("checkInfantryName failed");
+    @Nested
+    @DisplayName("Combat Stats")
+    class combatStats {
+        @Test
+        @DisplayName("Testing Defence and attack")
+        public void checkFighting() {
+            try {
+                int originalHealth = 150;
+                Unit infantryDefender = new InfantryUnit("Town Guard", originalHealth);
+                Unit infantryAggressor = new InfantryUnit("Bandit", 100);
+                infantryAggressor.attack(infantryDefender);
+                assertTrue((infantryDefender.getHealth() < originalHealth));
+            } catch (Exception e) {
+                fail("checkInfantryName failed");
+            }
         }
     }
 }
