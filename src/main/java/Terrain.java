83c223b43f40472a80d222baef34144caffbdb60
flippi321 <90700334+flippi321@users.noreply.github.com>
2022-04-19 20:38:24 +0200

    Removed unnecessary values and tests for Terrain

--- src/main/java/Terrain.java
+++ src/main/java/Terrain.java
@@ -1,12 +1,5 @@
 public enum Terrain {
-    HILL(4),
-    PLAINS(4),
-    FOREST(4);
-    int bonus;
-
-    Terrain(int bonus) {
-        this.bonus = bonus;
-    }
-
-
+    HILL(),
+    PLAINS(),
+    FOREST()
 }
--- src/test/java/TerrainTest.java
+++ src/test/java/TerrainTest.java
@@ -2,30 +2,6 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TerrainTest {
-
-    Terrain plains = Terrain.PLAINS;
-
-    @Test
-    @DisplayName("Testing Hill value")
-    public void checkHillValue(){
-        Terrain hill = Terrain.HILL;
-        assertEquals(4, hill.bonus);
-    }
-
-    @Test
-    @DisplayName("Testing Forest value")
-    public void checkForestValue(){
-        Terrain forest = Terrain.FOREST;
-        assertEquals(4, forest.bonus);
-    }
-
-    @Test
-    @DisplayName("Testing Hill value")
-    public void checkPlainsValue(){
-        Terrain plains = Terrain.PLAINS;
-        assertEquals(4, plains.bonus);
-    }
-
     @Test
     @DisplayName("Testing Equals Method")
     public void checkEqualMethod(){
